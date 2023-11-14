d1d52e9526049042fa7a5e3823dffad81d784afe
yuyt2012 <yuyt2012@gmail.com>
2023-11-14 13:07:28 +0900

    readline()을 이용해 문자열을 입력받는 클래스입니다.

---
+++ src/main/java/christmas/view/inputview/InputView.java
@@ -0,0 +1,9 @@
+package christmas.view.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputData() {
+        return Console.readLine();
+    }
+}
