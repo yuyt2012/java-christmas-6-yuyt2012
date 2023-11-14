5eb1fe0fb5170e2130918c95951062a49f34fae1
yuyt2012 <yuyt2012@gmail.com>
2023-11-14 13:20:07 +0900

    프로그램을 실행시키기 위한 main

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.ProjectController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ProjectController projectController = new ProjectController();
+        projectController.run();
     }
 }
