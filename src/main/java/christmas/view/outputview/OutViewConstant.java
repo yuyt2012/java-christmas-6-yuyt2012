381f2f180a2160c7977c0dd114c91fdb98264c28
yuyt2012 <yuyt2012@gmail.com>
2023-11-14 13:12:01 +0900

    OutputView클래스에서 사용될 상수들을 모아놨습니다.

---
+++ src/main/java/christmas/view/outputview/OutViewConstant.java
@@ -0,0 +1,25 @@
+package christmas.view.outputview;
+
+public class OutViewConstant {
+    public static final String PREFACE = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n";
+    public static final String NOTICE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    public static final String ORDER_MENU = "<주문 메뉴>\n";
+    public static final String BEFORE_TOTAL_PRICE = "<할인 전 총주문 금액>\n";
+    public static final String GIVEAWAY = "<증정 메뉴>\n";
+    public static final String NOTHING = "없음\n";
+    public static final String BENEFIT = "<혜택 내역>\n";
+    public static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인: %,d원\n";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인: %,d원\n";
+    public static final String WEEKEND_DISCOUNT = "주말 할인: %,d원\n";
+    public static final String STARDAY_DISCOUNT = "특별 할인: %,d원\n";
+    public static final String GIVEAWAY_DISCOUNT = "증정 이벤트: %,d원\n";
+    public static final String TOTAL_BENEFIT_PRICE = "<총혜택 금액>\n";
+    public static final String TOTAL_DISCOUNT = "%,d원\n";
+    public static final String EXPECT_PAYMENT = "<할인 후 예상 결제 금액>\n";
+    public static final String EXPECT_PAYMENT_PRICE = "%,d원\n";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>\n";
+
+    public OutViewConstant() {
+
+    }
+}
