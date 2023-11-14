e4aa0ea8a53aa3cba65b632e39cbd9cfabb17478
yuyt2012 <yuyt2012@gmail.com>
2023-11-14 13:16:52 +0900

    이 프로젝트에서 사용될 상수들을 모아놓은 클래스입니다.

---
+++ src/main/java/christmas/ProjectConstant.java
@@ -0,0 +1,17 @@
+package christmas;
+
+public class ProjectConstant {
+    public static final int YEAR = 2023;
+    public static final int MONTH = 12;
+    public static final int START_DATE = 1;
+    public static final int END_DATE = 31;
+    public static final int MIN_MENU = 1;
+    public static final int MAX_ORDER = 20;
+    public static final int CHRISTMAS = 25;
+    public static final int MINIMUM_GIVEAWAY_PRICE = 120_000;
+    public static final int MINIMUM_EVENT_PRICE = 10_000;
+
+    public ProjectConstant() {
+
+    }
+}
